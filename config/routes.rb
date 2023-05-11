5b301b76e91808aaaf73f61b0493e1a889aa01b7
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-11 12:10:17 +0000

    meshiterro_config

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to: "homus#top"
+  get 'meshiterros/new'
+  get 'meshiterros/index'
+  get 'meshiterros/show'
+  get 'meshiterros/edit'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
