9d390a472c63810cb25e9580c348a4ccc6646400
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-15 14:52:19 +0000

    meshiterro_app

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -8,6 +8,10 @@ class PostImage < ApplicationRecord
   
   has_many :favorites, dependent: :destroy
   
+  validates :shop_name, presence: true
+  
+  validates :image, presence: true
+  
   
   def get_image
     unless image.attached?
