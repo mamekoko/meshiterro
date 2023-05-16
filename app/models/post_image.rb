9400fea22453129b2892aff0ea57398e08b68eff
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-16 06:12:54 +0000

    meshiterro_app

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,18 +1,21 @@
 class PostImage < ApplicationRecord
-  
+
   has_one_attached :image
-  
+
   belongs_to :user
-  
+
   has_many :post_comments, dependent: :destroy
-  
+
   has_many :favorites, dependent: :destroy
-  
+
   validates :shop_name, presence: true
-  
+
   validates :image, presence: true
-  
-  
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -20,5 +23,5 @@ class PostImage < ApplicationRecord
     end
     image
   end
-  
+
 end
